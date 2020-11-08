6a3c466db2f9b560f1809ddfafdde34550773f0e
David <davemgj@gmail.com>
2020-11-08 13:01:33 -0800

    nav styling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import "../styles/Nav.scss";
+// import { Link } from "react-router-dom";
 
 const Nav = () => {
-  return <div></div>;
+  return (
+    <>
+      <nav>
+        <input type="checkbox" id="check" />
+        <label for="check" className="checkbtn">
+          <i className="fas fa-bars"></i>
+        </label>
+        <label className="logo">David Jardine</label>
+        <ul>
+          <li>
+            <a className="active" href="#">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Services</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
 };
 
 export default Nav;
