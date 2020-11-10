0e8f85632b2c8486f7ba2ff7d4c75395c9d87959
David <davemgj@gmail.com>
2020-11-09 17:35:59 -0800

    create nav with responsive behaviour - burger div when on mobile with nice animations

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,37 @@
 import React from "react";
 // import { Link } from "react-router-dom";
+import navSlide from "../helpers/navSlide";
 import "../styles/Nav.scss";
 
 const Nav = () => {
-  return <></>;
+  return (
+    <>
+      <nav>
+        <div className="logo">
+          <h4>DJ</h4>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Projects</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+        <div onClick={() => navSlide()} className="burger">
+          <div className="line1"></div>
+          <div className="line2"></div>
+          <div className="line3"></div>
+        </div>
+      </nav>
+    </>
+  );
 };
 
 export default Nav;
---
+++ src/helpers/navSlide.js
@@ -0,0 +1,19 @@
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  burger.classList.toggle("toggle");
+  nav.classList.toggle("nav-active");
+  navLinks.forEach((link, index) => {
+    if (link.style.animation) {
+      link.style.animation = "";
+    } else {
+      link.style.animation = `navLinkFade 0.5s ease forwards ${
+        index / 7 + 0.5
+      }s`;
+    }
+  });
+};
+
+export default navSlide;
