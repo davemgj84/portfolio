1f4634a59a8dac851ab0c996f0be596f489bc889
David <davemgj@gmail.com>
2020-11-09 16:04:55 -0800

    start new with different approach

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,35 +1,9 @@
 import React from "react";
-import "../styles/Nav.scss";
 // import { Link } from "react-router-dom";
+import "../styles/Nav.scss";
 
 const Nav = () => {
-  return (
-    <>
-      <nav>
-        <input type="checkbox" id="check" />
-        <label for="check" className="checkbtn">
-          <i className="fas fa-bars"></i>
-        </label>
-        <label className="logo">David Jardine</label>
-        <ul>
-          <li>
-            <a className="active" href="#">
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">Services</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
-          </li>
-        </ul>
-      </nav>
-    </>
-  );
+  return <></>;
 };
 
 export default Nav;
