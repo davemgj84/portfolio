b56980ae50ffa768ebf9e8af4344953c5e57ba1a
David <davemgj@gmail.com>
2020-11-18 03:10:30 -0800

    create Typewriter component for animated text - install react-spring

---
+++ src/components/Typewriter.jsx
@@ -0,0 +1,31 @@
+import React, { useMemo } from "react";
+import { animated, useTransition } from "react-spring";
+
+const Typewriter = ({ message }) => {
+  const items = useMemo(
+    () =>
+      message.split("").map((letter, index) => ({
+        item: letter,
+        key: index,
+      })),
+    [message]
+  );
+  const transitions = useTransition(items, (item) => item.key, {
+    trail: 35,
+    from: { display: "none" },
+    enter: { display: "" },
+  });
+  return (
+    <>
+      {transitions.map(({ item, props, key }) => {
+        return (
+          <animated.span key={key} style={props}>
+            {item.item}
+          </animated.span>
+        );
+      })}
+    </>
+  );
+};
+
+export default Typewriter;
