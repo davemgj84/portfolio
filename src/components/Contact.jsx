ca38c356a10204658ab2ede5f9af1946b5914883
David <davemgj@gmail.com>
2020-11-21 18:29:40 -0800

    responsive

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,44 +5,45 @@ const Contact = () => {
   return (
     <>
       <section className="contacts">
-        <h1>Contact</h1>
+        <div>
+          <h1>Contact</h1>
+          <h3>
+            <a className="phone" href="tel:778-998-8368">
+              <i className="fas fa-mobile-alt fa-2x"></i> (778) 998-8368
+            </a>
+          </h3>
 
-        <h3>
-          <a className="phone" href="tel:778-998-8368">
-            <i className="fas fa-mobile-alt fa-2x"></i> (778) 998-8368
-          </a>
-        </h3>
-
-        <h3>
-          <a
-            className="email"
-            href="mailto:davemgj@gmail.com"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <i className="far fa-envelope fa-2x"></i> davemgj@gmail.com
-          </a>
-        </h3>
-        <h3>
-          <a
-            className="github"
-            href="https://github.com/davemgj84"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <i className="fab fa-github fa-2x"></i> Github
-          </a>
-        </h3>
-        <h3>
-          <a
-            className="linked-in"
-            href="https://www.linkedin.com/in/david-jardine-a639891b7/"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <i className="fab fa-linkedin fa-2x"></i> LinkedIn
-          </a>
-        </h3>
+          <h3>
+            <a
+              className="email"
+              href="mailto:davemgj@gmail.com"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <i className="far fa-envelope fa-2x"></i> davemgj@gmail.com
+            </a>
+          </h3>
+          <h3>
+            <a
+              className="github"
+              href="https://github.com/davemgj84"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <i className="fab fa-github fa-2x"></i> Github
+            </a>
+          </h3>
+          <h3>
+            <a
+              className="linked-in"
+              href="https://www.linkedin.com/in/david-jardine-a639891b7/"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <i className="fab fa-linkedin fa-2x"></i> LinkedIn
+            </a>
+          </h3>
+        </div>
       </section>
     </>
   );
