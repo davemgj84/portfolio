1a8323851624e716567c99cee824ad14422d0630
David <davemgj@gmail.com>
2020-11-25 13:30:17 -0800

    rework landing page to work on iphones

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,22 +7,24 @@ const About = () => {
   return (
     <>
       <section className="about">
-        <div>
-          <img className="profile-image" src={profile} alt="David" />
-        </div>
-        <div className="bio">
-          <h1>David Jardine</h1>
-          <p className="bio-text">
-            I am a Full Stack Junior Web Developer with a background as an
-            artist. I offer many perspectives and modes of thinking with my
-            diverse background. I have always loved creating and building
-            interactive projects and collaborating with a team. I enjoy critical
-            thinking, problem solving, and creativity in my work. I recently
-            finished the Web Development Bootcamp at Lighthouse Labs and am
-            eager to begin my journey as a developer. Check out my{" "}
-            <Link to={"/projects"}>projects</Link> and{" "}
-            <Link to={"/contact"}>contact</Link> me today!
-          </p>
+        <div className="container">
+          <div>
+            <img className="profile-image" src={profile} alt="David" />
+          </div>
+          <div className="bio">
+            <h1>David Jardine</h1>
+            <p className="bio-text">
+              I am a Full Stack Junior Web Developer with a background as an
+              artist. I offer many perspectives and modes of thinking with my
+              diverse background. I have always loved creating and building
+              interactive projects and collaborating with a team. I enjoy
+              critical thinking, problem solving, and creativity in my work. I
+              recently finished the Web Development Bootcamp at Lighthouse Labs
+              and am eager to begin my journey as a developer. Check out my{" "}
+              <Link to={"/projects"}>projects</Link> and{" "}
+              <Link to={"/contact"}>contact</Link> me today!
+            </p>
+          </div>
         </div>
       </section>
     </>
