6166e0654de183097a657b2085975ee89fdcc636
David <davemgj@gmail.com>
2020-11-26 00:31:00 -0800

    updated app.js to allow pages to have scroll restoration when navigating between links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-import React from "react";
-import { BrowserRouter, Route, Redirect } from "react-router-dom";
+import React, { useEffect } from "react";
+import {
+  BrowserRouter,
+  Route,
+  Redirect,
+  useLocation,
+  withRouter,
+} from "react-router-dom";
 import "./index.css";
 import Nav from "./components/Nav";
 import Home from "./components/Home";
@@ -8,17 +14,28 @@ import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 
 function App() {
+  function _ScrollToTop(props) {
+    const { pathname } = useLocation();
+    useEffect(() => {
+      window.scrollTo(0, 0);
+    }, [pathname]);
+    return props.children;
+  }
+  const ScrollToTop = withRouter(_ScrollToTop);
+
   return (
     <div className="App">
       <BrowserRouter>
-        <Nav />
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
-        <Route path="/home" component={Home} />
-        <Route path="/skills" component={Skills} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/contact" component={Contact} />
+        <ScrollToTop>
+          <Nav />
+          <Route exact path="/">
+            <Redirect to="/home" />
+          </Route>
+          <Route path="/home" component={Home} />
+          <Route path="/skills" component={Skills} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/contact" component={Contact} />
+        </ScrollToTop>
       </BrowserRouter>
     </div>
   );
