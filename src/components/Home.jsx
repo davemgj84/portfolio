d5671377b9ec217356fa5dbb64467eebb42b1e77
davemgj84 <davemgj@gmail.com>
2024-01-19 15:05:47 -0800

    add comment

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -35,6 +35,14 @@ export default function Home() {
               energy! Feel free to check out my{" "}
               <Link to={"/projects"}>projects</Link> and{" "}
               <Link to={"/contact"}>contact</Link> me today!
+              <br />
+              <br />
+              Please note that many of the projects date back to 2020/2021. I
+              have been employed full-time at Clir Renewables Inc. for the past
+              ~3 years and have not had much personal time to start new side
+              projects. I am excited to share that I am currently initiating new
+              projects, focusing on TypeScript/React and other technologies I
+              wish to explore, which will better showcase my current skillset.
             </p>
           </div>
         </div>
